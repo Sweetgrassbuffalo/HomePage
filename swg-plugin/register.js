7d7b33220e818acbf21b1f6315dcc7b672a20381
Kenneth Beartusk <DaddyTusk@localhost.localdomain>
2017-12-11 11:56:36 -0700

    new plugin to implement sweetgrassbuffalo.com style changes

---
+++ swg-plugin/register.js
@@ -0,0 +1,9 @@
+import { Reaction } from "/server/api";
+
+// Register package as ReactionCommerce package
+Reaction.registerPackage({
+  label: "Hydrotik",
+  name: "hydrotik",
+  icon: "fa fa-vine",
+  autoEnable: true,
+});
